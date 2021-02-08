4ed36c682cd77b3a6f19a6ee0827af4fe8a6b356
leolongpy <leolongpy@qq.com>
2021-02-08 16:14:07 +0800

    媒体数量

--- day5/slice.go
+++ day5/slice.go
@@ -4,14 +4,21 @@ import "fmt"
 
 func main() {
 	m1 := map[int]string{1: "zs", 2: "ls"}
-	//修改
 	m1[1] = "ww"
 	m1[3] = "zhaosi"
 	fmt.Println(m1)
+	//遍历
+	for k, v := range m1 {
+		fmt.Printf("%d----->%s\n", k, v)
+	}
 
-	//创建
-	m2 := make(map[int]string, 8)
-	m2[0] = "aaaa"
-	m2[1] = "bbbb"
-	fmt.Println(m2[0])
+	for k := range m1 {
+		fmt.Printf("%d---->%s\n", k, m1[k])
+	}
+
+	value,ok :=m1[5]
+	fmt.Println("value=",value,",ok=",ok)
+
+	delete(m1,1)
+	fmt.Println(m1)
 }
